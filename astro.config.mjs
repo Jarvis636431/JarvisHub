a4c2d4e9c94bb81db890b065d05a944663513fd6
Jarvis <mengda636431@gmail.com>
2025-11-21 01:04:50 +0800

    [fea] 构建了全对称由路由组织的多语言系统

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,9 @@ export default defineConfig({
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
   },
   integrations: [
     mdx(),
